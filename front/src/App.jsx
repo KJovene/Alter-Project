81824dca64fcdc70781e37c3cc202df8b5285d79
Kévin <jovene.kevin@gmail.com>
2025-03-25 12:07:28 +0100

    :lipstick: Fix CSS

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Candidature from './components/candidature/candidature.jsx';
+import Accueil from './components/accueil/accueil.jsx';
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Accueil />} />
         <Route path="/candidature" element={<Candidature />} />
       </Routes>
     </Router>
---
+++ front/src/components/accueil/accueil.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import './accueil.css';
+
+const Accueil = () => {
+
+
+  return (
+
+    <div className="bodyAccueil">
+      <h1>Bienvenue sur vos candidatures</h1>
+      <Link to="/candidature">Ajouter une candidature</Link>
+    </div>
+  );
+}
+
+export default Accueil;
