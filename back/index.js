c776df22ba4c26abfe492cc5cc074e129912de58
MarcheschiThomas <marcheschi.thomas@gmail.com>
2025-03-25 12:16:19 +0100

    :truck: Ajout router

--- back/index.js
+++ back/index.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import dbConnect from "./db/dbconnect.js";
-
+import router from "./routes/router.js";
 
 
 dotenv.config();
@@ -13,6 +13,9 @@ const PORT = process.env.PORT || 4732;
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(router)
+
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ back/routes/router.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { getAllCandidates, postCandidate, putCandidate, deleteCandidate } from '../services/candidat.service.js';
+
+const router = express.Router();
+
+router.post('/candidature' , postCandidate);
+router.get('/candidature', getAllCandidates);
+router.delete('/candidature/:id', deleteCandidate);
+router.put('/candidature/:id', putCandidate);
+
+export default router;
