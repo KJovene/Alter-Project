570d269a5f2a51ef1b49b28c1a1a7dcc559fe701
Laurent Dubois <laurent.dubois91710@gmail.com>
2025-03-25 10:38:30 +0100

    :wrench: Dependencies

---
+++ back/db/dbconnect.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const dbconnect = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        })
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
+    }
+    }
