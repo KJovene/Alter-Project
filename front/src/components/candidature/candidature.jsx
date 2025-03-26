2fe7b2cd021515229f3b46d7aa991790f26323a7
Laurent Dubois <laurent.dubois91710@gmail.com>
2025-03-26 13:33:13 +0100

    :construction: Progression post

--- front/src/components/candidature/candidature.jsx
+++ front/src/components/candidature/candidature.jsx
@@ -1,79 +1,61 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import './candidature.css';
 import axiosInstance from '../../axios';
 
 
 const Candidature = () => {
-  const [formData, setformData] = useState('attente');
-  // const [formEntreprise, setformEntreprise] = useState('');
-  // const [formPoste, setformPoste] = useState('');
-  // const [formLien, setformLien] = useState('');
-  // const [formDate, setformDate] = useState('');
+  const [formEntreprise, setformEntreprise] = useState('');
+  const [formPoste, setformPoste] = useState('');
+  const [formLien, setformLien] = useState('');
+  const [formDate, setformDate] = useState('');
+  const [formStatus, setformStatus] = useState('');
   
   const handleChange = (event) => {
-    setformData(event.target.formData);
+    setformStatus(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
+    const data = await axiosInstance.post("/candidature", {
+      entreprise : formEntreprise,
+      poste : formPoste,
+      lien : formLien,
+      date : formDate,
+      status : formStatus
+    })
+    .json(data)
     event.preventDefault();
   };
-
-  useEffect(() => {
-    const fetchCandidature = async () => {
-      try {
-        const response = await axiosInstance.get("/liste")
-        console.log(response)
-      } catch(err){
-        console.log(err)
-      }
-    }
-    fetchCandidature()
-  },[])
-
-  // useEffect(() => {
-  //   const postCandidature = async () => {
-  //     try {
-  //       const post = await axiosInstance.post("/candidature",
-  //         {
-  //           entreprise: "entreprise"
-  //         }
-  //       )
-  //       console.log(response)
-  //     } catch(err){
-  //       console.log(err)
-  //     }
-  //   }
-  //   postCandidature()
-  // },[])
+  
+  
 
   return (
     <div className="bodyCandidature">
       <form method="POST" onSubmit={handleSubmit}>
         <label className="label">
           Entreprise 
-          <input type="text" name="entreprise" placeholder="Microsoft"/>
+          <input type="text" name="entreprise" placeholder="Microsoft" onChange = {e => setformEntreprise(e.target.value)} value={formEntreprise} />
         </label>
         <label className="label">
           Poste
-          <input type="text" name="poste" placeholder="CTO"/>
+          <input type="text" name="poste" placeholder="CTO" onChange = {e => setformPoste(e.target.value)} value={formPoste}/>
         </label>
         <label className="label">
           Lien
-          <input type="text" name="lien" />
+          <input type="text" name="lien" onChange = {e => setformLien(e.target.value)} value={formLien} />
         </label>
         <label className="label">
           Date de l'inscription
-          <input type="date" name="date" />
+          <input type="date" name="date" onChange = {e => setformDate(e.target.value)} value={formDate} />
         </label>
         <label>
           Statut :
-          <select name={formData} onChange={handleChange}>
-            <option name="attente">En attente</option>
+          <select name="status" onChange={handleChange} value={formStatus}>
+            <option defaultValue="attente" name="attente">En attente</option>
             <option name="accepté">Accepté</option>
             <option name="refusé">Refusé</option>
           </select>
         </label>
-        <input type="submit" name="Envoyer" />
+        <input type="submit" />
       </form>
     </div>
   );
